30a5d9d9e0a5eb6b7601a412caf4bbc3174a5c56
Alan Tran <contactalantran@gmail.com>
2020-11-23 22:49:23 -0500

    added express new campground validation

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -41,6 +42,7 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 app.post('/campgrounds', catchAsync(async(req, res, next) => {
+    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -68,8 +70,13 @@ app.delete('/campground/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds');
 }))
 
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
 app.use((err, req, res, next) => {
-    res.send("oh boy, something went wrong!")
+    const { statusCode = 500, message= 'Something went wrong!' } = err;
+    res.status(statusCode).send(message);
 })
 
 app.listen(3000, () => {
