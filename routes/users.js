cf67133990850dc9024cc0c09396090059f1fb54
Alan Tran <contactalantran@gmail.com>
2020-11-27 17:28:43 -0500

    register route logic

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,18 @@ router.get('/register', (req, res) => {
     res.render('users/register')
 })
 
-router.post('/register', async(req, res) => {
-    res.send(req.body);
-})
+router.post('/register', catchAsync(async(req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        console.log(registeredUser);
+        req.flash('success', 'welcome to yelpcamp!');
+        res.redirect('/campgrounds');
+    } catch(e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+}))
 
 module.exports = router;
