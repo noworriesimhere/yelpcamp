aad18459eeb9e82d2dbc0fb3ed57a19ffeb20cfb
Alan Tran <contactalantran@gmail.com>
2020-11-27 16:42:26 -0500

    make register form

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -61,14 +62,9 @@ app.use((req, res, next) => {
     next();
 })
 
-app.get('/fakeUser', async (req, res) => {
-    const user = new User({ email: 'alan@alantran.tech', username: 'alan' })
-    const newUser = await User.register(user, 'chicken');
-    res.send(newUser);
-})
-
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
     res.render('home');
---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+
+router.get('/register', (req, res) => {
+    res.render('users/register')
+})
+
+router.post('/register', async(req, res) => {
+    res.send(req.body);
+})
+
+module.exports = router;
