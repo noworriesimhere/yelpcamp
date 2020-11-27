652e3d1e31535a213d363c84b7d88924c61903ad
Alan Tran <contactalantran@gmail.com>
2020-11-27 17:54:27 -0500

    make login routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-
+const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
-const ExpressError = require('../utils/ExpressError');
+
 
 router.get('/register', (req, res) => {
     res.render('users/register')
@@ -22,5 +22,14 @@ router.post('/register', catchAsync(async(req, res) => {
         res.redirect('register');
     }
 }))
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'welcome back!');
+    res.redirect('/campgrounds');
+})
 
 module.exports = router;
