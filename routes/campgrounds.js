89f88f22178b9541a24e85476d16d6aba9bed75f
Alan Tran <contactalantran@gmail.com>
2020-11-27 22:33:08 -0500

    add logout and update navbar

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -41,7 +41,7 @@ router.get('/:id', catchAsync(async (req, res) => {
     res.render('campgrounds/show', { campground });
 }));
 
-router.get('/:id/edit', catchAsync(async(req, res) => {
+router.get('/:id/edit', isLoggedIn, catchAsync(async(req, res) => {
     const campground = await Campground.findById(req.params.id);
     if(!campground){
         req.flash('error', 'Cannot find that campground!');
