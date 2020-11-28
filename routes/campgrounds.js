046210308ce6ffded7b0e0516a344990c135d812
Alan Tran <contactalantran@gmail.com>
2020-11-28 09:42:12 -0500

    add backend campground permissions

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -52,9 +52,14 @@ router.get('/:id/edit', isLoggedIn, catchAsync(async(req, res) => {
     res.render('campgrounds/edit', { campground })
 }))
 
-router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission for that!');
+        return res.redirect(`/campgrounds/${id}`)
+    }
+    const camp = await Campground.findByIdAndUpdate(id, {...req.body.campground});
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`); 
 }))
