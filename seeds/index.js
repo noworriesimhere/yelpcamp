72f61caa604812448702afc1453e05d7db6a10a2
Alan Tran <contactalantran@gmail.com>
2020-11-29 14:47:08 -0500

    add upload to edit page

--- seeds/index.js
+++ seeds/index.js
@@ -23,7 +23,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '5fc16c87ef9a4a2345c412dd',
+            author: '5fc2dd30e1b2a54a509f58ef',
             title: `${sample(descriptors)} ${sample(places)}`,
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores perferendis repudiandae voluptatem eaque ab fugit sint rem eum, cumque officiis temporibus soluta. Fuga sapiente debitis assumenda, atque hic accusamus cum.',
