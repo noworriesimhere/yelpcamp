ee821d577b297a85dde9c4df9185f8bdea119ed0
Alan Tran <contactalantran@gmail.com>
2020-11-29 00:00:38 -0500

    uploading to cloudinary

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,7 +5,8 @@ const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'});
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
