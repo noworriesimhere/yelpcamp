d8761cfec683c8766b485b12a08178ebc4457c37
Alan Tran <contactalantran@gmail.com>
2020-11-29 23:14:21 -0500

    centering map

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,6 +2,10 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-center: [-74.5, 40], // starting position [lng, lat]
-zoom: 9 // starting zoom
+center: campground.geometry.coordinates, // starting position [lng, lat]
+zoom: 8 // starting zoom
 });
+
+const marker = new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map);
